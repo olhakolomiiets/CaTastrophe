a8ae721ee6288aac8799d9ccac2529e84d812a22
Stas <stdanshin@gmail.com>
2023-08-07 20:24:22 +0300

    Added marks to cat icons in shop, to inform that they already bought

---
+++ Assets/Scripts/AlreadyBoughtCatShop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlreadyBoughtCatShop : MonoBehaviour
+{
+    public string ppNamePower;
+    private int purch;
+    [SerializeField] private GameObject doneIco;
+    bool isDone = false;
+    void Start()
+    {
+        IsBought();
+    }
+    private void Update()
+    {
+        if (isDone == false)
+        {
+            IsBought();
+        }
+    }
+    public void IsBought()
+    {
+        if (doneIco != null)
+        {
+            purch = PlayerPrefs.GetInt(ppNamePower, 0);
+            if (purch == 0)
+            {
+                doneIco.SetActive(false);
+            }
+            else
+            {
+                doneIco.SetActive(true);
+                isDone = true;
+            }
+        }
+        
+    }
+}
