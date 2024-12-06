e1dba041c0c099ec64c34ae3274df3cd55a37f1a
Stas <stdanshin@gmail.com>
2024-12-06 18:54:37 +0200

    Added Start scene and load script

---
+++ Assets/Scripts/StartScene/LoadingScene.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField] private GameObject LoadingScreen;
+    [SerializeField] private Image LoadingBarFill;
+    [SerializeField] private int sceneIdToLoad;
+
+   private void Start()
+    {
+        LoadScene(sceneIdToLoad);
+    }
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            LoadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+        }
+    }
+}
